23c92bcd88bbbd3855ec472b99d8719795a1df8b
Igor <nekto.korobko@gmail.com>
2022-02-05 00:59:50 +0300

    Create function get_lessons: Accepts the name of the subject, the year and the letter of the student's class, returns a set of found objects of the Lesson model.
    Create function create_commendation:
    Accepts the student's name, the name of the lesson subject and creates an object of the Recommendation model with the
    text of praise on a random day of lessons held, run

--- scripts.py
+++ scripts.py
@@ -1,5 +1,5 @@
 import os
-
+import random
 import django
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
@@ -9,6 +9,9 @@ from datacenter.models import (
     Schoolkid,
     Chastisement,
     Mark,
+    Lesson,
+    Subject,
+    Commendation,
 )
 
 
@@ -33,3 +36,42 @@ def fix_marks(schoolkid: Schoolkid):
 def remove_chastisements(schoolkid: Schoolkid):
     chastisement = Chastisement.objects.filter(schoolkid=schoolkid)
     chastisement.delete()
+
+
+def get_lessons(title: str, year: str, letter: str):
+    try:
+        lesson = Lesson.objects.filter(
+            group_letter=letter,
+            year_of_study=year,
+            subject=title).order_by('date')
+        return lesson
+    except Lesson.ObjectDoesNotExist:
+        print(f'Предмет {title} не найден')
+
+
+def create_commendation(schoolkid: Schoolkid, lesson_title: str):
+    year_of_study = schoolkid.year_of_study
+    group_letter = schoolkid.group_letter
+    commendations = [
+        'Прекрасно!',
+        'Молодец!',
+        'Талантливо!',
+        'Хорошо!',
+        'Великолепно!',
+        'Очень хороший ответ!',
+        'Талантливо!',
+        'Отлично!',
+        'Красава',
+        'Здорово!'
+    ]
+    try:
+        subject = Subject.objects.get(title=lesson_title, year_of_study=year_of_study)
+        lesson = random.choice(get_lessons(subject, year_of_study, group_letter))
+        Commendation.objects.create(
+            schoolkid=schoolkid,
+            subject=lesson.subject,
+            text=random.choice(commendations),
+            created=lesson.date,
+            teacher=lesson.teacher)
+    except Subject.DoesNotExist:
+        print(f"Subject {lesson_title} not found.")
